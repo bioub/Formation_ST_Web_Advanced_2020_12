01e2361e4c714f3513d18e4bf989ef372ab0e848
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2021-01-13 12:30:48 +0100

    Correction TodoList

--- React/hello-react/src/TodoList.js
+++ React/hello-react/src/TodoList.js
@@ -24,34 +24,52 @@ class TodoList extends Component {
   handleSubmit = (event) => {
     event.preventDefault();
     this.setState({
+      newTodo: "",
       todos: [
         {
           id: Math.random(),
           title: this.state.newTodo,
           completed: false,
         },
-        ...this.state.todos,
+        ...this.state.todos, // SPREAD (conversion du tableau en une liste d'élements)
       ],
     });
+    // TODO envoyer la requête à Node.js
   };
   handleDelete(todo) {
-    console.log(todo);
     /*
     Exercice 1 :
     Créer un nouveau tableau sans la todo reçu en paramètre (.filter)
     et le passer à this.setState()
     */
+    this.setState({
+      todos: this.state.todos.filter((t) => t !== todo),
+    });
   }
   handleToggle = (event) => {
     const checked = event.target.checked;
 
+    // this.setState({
+    //   todos: this.state.todos.map((t) => {
+    //     return {
+    //       ...t,
+    //       completed: checked
+    //     };
+    //   }),
+    // });
+    this.setState({
+      todos: this.state.todos.map((t) => ({
+        ...t,
+        completed: checked,
+      })),
+    });
     /*
     Exercice 2 :
     Créer un nouveau tableau ou toutes les clés completed (.map)
     reçoivent checked
     et le passer à this.setState()
     */
-  }
+  };
 
   /*
   Exercice 3
@@ -63,22 +81,71 @@ class TodoList extends Component {
   Au double clic de la balise span, la transformer en balise input
   et stocker la valeur saisie dans le tableau
   */
-  handleCheckboxChange = (event, t) => {
+  handleCheckboxChange = (todo) => {
+    // this.setState({
+    //   todos: this.state.todos.map((t) => {
+    //     if (todo !== t) {
+    //       return t;
+    //     }
 
-  }
+    //     return {
+    //       ...t,
+    //       completed: !t.completed,
+    //     };
+    //   }),
+    // });
 
-  handleDoubleClick = (event) => {
+    this.setState({
+      todos: this.state.todos.map((t) =>
+        t !== todo ? t : { ...t, completed: !t.completed }
+      ),
+    });
+  };
 
-  }
+  handleToggleEdit = (todo) => {
+    this.setState({
+      todos: this.state.todos.map((t) =>
+        t !== todo ? t : { ...t, edit: !t.edit }
+      ),
+    });
+  };
+
+  handleTitleChange = (todo, title) => {
+    this.setState({
+      todos: this.state.todos.map((t) => (t !== todo ? t : { ...t, title })),
+    });
+  };
 
   render() {
-    const todosJsx = this.state.todos.map((t) => (
-      <div key={t.id}>
-        <input type="checkbox" checked={t.completed} onChange={(event) => this.handleCheckboxChange(event, t)} />
-        <span onDoubleClick={this.handleDoubleClick}>{t.title}</span>
-        <button onClick={() => this.handleDelete(t)}>-</button>
-      </div>
-    ));
+    const todosJsx = this.state.todos.map((t) => {
+      let titleJsx;
+      if (!t.edit) {
+        titleJsx = (
+          <span onDoubleClick={() => this.handleToggleEdit(t)}>{t.title}</span>
+        );
+      } else {
+        titleJsx = (
+          <input
+            autoFocus
+            value={t.title}
+            onChange={(event) => this.handleTitleChange(t, event.target.value)}
+            onBlur={() => this.handleToggleEdit(t)}
+          />
+        );
+      }
+
+      return (
+        <div key={t.id}>
+          <input
+            type="checkbox"
+            checked={t.completed}
+            onChange={() => this.handleCheckboxChange(t)}
+          />
+          {titleJsx}
+          <button onClick={() => this.handleDelete(t)}>-</button>
+        </div>
+      );
+    });
 
     return (
       <div className="TodoList">
